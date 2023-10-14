a9844a2999ac8593e8e840e420a8c1a4e9e6824e
Justin Rinehuls <jrinehuls@gmail.com>
2023-10-14 14:52:48 -0400

    Initial commit

---
+++ src/main/java/com/jrinehuls/mimeemail/MimeEmailApplication.java
@@ -0,0 +1,13 @@
+package com.jrinehuls.mimeemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimeEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimeEmailApplication.class, args);
+	}
+
+}
