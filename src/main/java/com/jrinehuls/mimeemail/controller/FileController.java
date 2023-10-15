a5f02791d7136c95f32dd08253d1d1b35452800a
Justin Rinehuls <jrinehuls@gmail.com>
2023-10-15 19:53:43 -0400

    Add haneler to return png

--- src/main/java/com/jrinehuls/mimeemail/controller/FileController.java
+++ src/main/java/com/jrinehuls/mimeemail/controller/FileController.java
@@ -4,6 +4,7 @@ import com.jrinehuls.mimeemail.model.B64File;
 import com.jrinehuls.mimeemail.service.FileService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
@@ -20,9 +22,20 @@ public class FileController {
 
     FileService fileService;
 
-    @PostMapping("/b64")
+    @PostMapping(value = "/b64")
     public ResponseEntity<ArrayList<B64File>> encodeFilesB64(@RequestParam(value = "files") MultipartFile[] files) {
         return new ResponseEntity<>(fileService.encodeFilesB64(files), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/png", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> encodeFilesB64Img(@RequestParam(value = "files") MultipartFile file) {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = file.getBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(bytes, HttpStatus.OK);
     }
 
 
